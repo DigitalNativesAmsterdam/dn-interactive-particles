0d389e52804ae1f237bd146fe663a70c1937ce7b
Mark Strefford <mark.strefford@gmail.com>
2022-06-16 18:28:32 +0100

    Interacting on pose estimation json

--- src/scripts/webgl/controls/InteractiveControls.js
+++ src/scripts/webgl/controls/InteractiveControls.js
@@ -1,7 +1,7 @@
 import EventEmitter from 'events';
 import * as THREE from 'three';
 import browser from 'browser-detect';
-
+import { points } from './points';
 import { passiveEvent } from '../../utils/event.utils.js';
 
 export default class InteractiveControls extends EventEmitter {
@@ -38,6 +38,10 @@ export default class InteractiveControls extends EventEmitter {
 		this.yStep = 0.0;
 		this.interactX = this.minX;
 		this.interactY = 0;
+
+		this.frame = 0;
+		this.num_frames = 147;
+		console.log(points);
 
 		this.enable();
 	}
@@ -151,63 +155,112 @@ export default class InteractiveControls extends EventEmitter {
 	}
 
 	onInteract() {
-		// const t = (e.touches) ? e.touches[0] : e;
-		// const touch = { x: t.clientX, y: t.clientY };
-		// console.log(touch);
-		//
-		// this.mouse.x = ((touch.x + this.rect.x) / this.rect.width) * 2 - 1;
-		// this.mouse.y = -((touch.y + this.rect.y) / this.rect.height) * 2 + 1;
-		if (this.interactX > 1) { return }
+		let interactPoints = points[this.frame.toString()];
 
-		this.mouse.x = this.interactX;
-		this.mouse.y = this.interactY;
+		for (var interactPoint of interactPoints) {
+			let point = {x: 0.0, y: 0.0};
 
-		this.interactX += this.xStep;
-		this.interactY += this.yStep;
+			point.x = interactPoint[0] * 2 - 1;
+			point.y = interactPoint[1] * 2 - 1;
+			console.log(point);
 
-		console.log(this.mouse);
+			this.raycaster.setFromCamera(point, this.camera);
 
-		this.raycaster.setFromCamera(this.mouse, this.camera);
+			const intersects = this.raycaster.intersectObjects(this.objects);
 
-		/*
-		// is dragging
-		if (this.selected && this.isDown) {
-			if (this.raycaster.ray.intersectPlane(this.plane, this.intersection)) {
-				this.emit('interactive-drag', { object: this.selected, position: this.intersection.sub(this.offset) });
-			}
-			return;
-		}
-		*/
+			if (intersects.length > 0) {
+				const object = intersects[0].object;
+				this.intersectionData = intersects[0];
 
-		const intersects = this.raycaster.intersectObjects(this.objects);
+				this.plane.setFromNormalAndCoplanarPoint(this.camera.getWorldDirection(this.plane.normal), object.position);
 
-		if (intersects.length > 0) {
-			const object = intersects[0].object;
-			this.intersectionData = intersects[0];
-
-			this.plane.setFromNormalAndCoplanarPoint(this.camera.getWorldDirection(this.plane.normal), object.position);
-
-			if (this.hovered !== object) {
-				console.log('this.hovered !== object => interactive-out & over');
-				this.emit('interactive-out', { object: this.hovered });
-				this.emit('interactive-over', { object });
-				this.hovered = object;
+				if (this.hovered !== object) {
+					// console.log('this.hovered !== object => interactive-out & over');
+					this.emit('interactive-out', { object: this.hovered });
+					this.emit('interactive-over', { object });
+					this.hovered = object;
+				}
+				else {
+					// console.log(object);
+					// console.log(this.intersectionData);
+					this.emit('interactive-move', { object, intersectionData: this.intersectionData });
+				}
 			}
 			else {
-				// console.log(object);
-				console.log(this.intersectionData);
-				this.emit('interactive-move', { object, intersectionData: this.intersectionData });
-			}
-		}
-		else {
-			this.intersectionData = null;
+				this.intersectionData = null;
 
-			if (this.hovered !== null) {
-				this.emit('interactive-out', { object: this.hovered });
-				this.hovered = null;
+				if (this.hovered !== null) {
+					this.emit('interactive-out', { object: this.hovered });
+					this.hovered = null;
+				}
 			}
 		}
+
+		this.frame++;
+		if (this.frame > this.num_frames) {this.frame = 0}
+
+
+
 	}
+
+	// onInteract2() {
+	// 	// const t = (e.touches) ? e.touches[0] : e;
+	// 	// const touch = { x: t.clientX, y: t.clientY };
+	// 	// console.log(touch);
+	// 	//
+	// 	// this.mouse.x = ((touch.x + this.rect.x) / this.rect.width) * 2 - 1;
+	// 	// this.mouse.y = -((touch.y + this.rect.y) / this.rect.height) * 2 + 1;
+	// 	if (this.interactX > 1) { return }
+	//
+	// 	this.mouse.x = this.interactX;
+	// 	this.mouse.y = this.interactY;
+	//
+	// 	this.interactX += this.xStep;
+	// 	this.interactY += this.yStep;
+	//
+	// 	console.log(this.mouse);
+	//
+	// 	this.raycaster.setFromCamera(this.mouse, this.camera);
+	//
+	// 	/*
+	// 	// is dragging
+	// 	if (this.selected && this.isDown) {
+	// 		if (this.raycaster.ray.intersectPlane(this.plane, this.intersection)) {
+	// 			this.emit('interactive-drag', { object: this.selected, position: this.intersection.sub(this.offset) });
+	// 		}
+	// 		return;
+	// 	}
+	// 	*/
+	//
+	// 	const intersects = this.raycaster.intersectObjects(this.objects);
+	//
+	// 	if (intersects.length > 0) {
+	// 		const object = intersects[0].object;
+	// 		this.intersectionData = intersects[0];
+	//
+	// 		this.plane.setFromNormalAndCoplanarPoint(this.camera.getWorldDirection(this.plane.normal), object.position);
+	//
+	// 		if (this.hovered !== object) {
+	// 			console.log('this.hovered !== object => interactive-out & over');
+	// 			this.emit('interactive-out', { object: this.hovered });
+	// 			this.emit('interactive-over', { object });
+	// 			this.hovered = object;
+	// 		}
+	// 		else {
+	// 			// console.log(object);
+	// 			console.log(this.intersectionData);
+	// 			this.emit('interactive-move', { object, intersectionData: this.intersectionData });
+	// 		}
+	// 	}
+	// 	else {
+	// 		this.intersectionData = null;
+	//
+	// 		if (this.hovered !== null) {
+	// 			this.emit('interactive-out', { object: this.hovered });
+	// 			this.hovered = null;
+	// 		}
+	// 	}
+	// }
 
 	onDown(e) {
 		this.isDown = true;
